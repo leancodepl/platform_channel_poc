e837cbd4e6d85bdd7e00486a7668309d19802046
Mateusz Wojtczak <mateusz.wojtczak@leancode.pl>
2022-11-04 16:10:56 +0100

    Add iOS impl

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,6 +3,31 @@ import Flutter
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
+  let json = """
+              {
+                "glossary": {
+                    "title": "example glossary",
+                "GlossDiv": {
+                        "title": "S",
+                  "GlossList": {
+                            "GlossEntry": {
+                                "ID": "SGML",
+                      "SortAs": "SGML",
+                      "GlossTerm": "Standard Generalized Markup Language",
+                      "Acronym": "SGML",
+                      "Abbrev": "ISO 8879:1986",
+                      "GlossDef": {
+                                    "para": "A meta-markup language, used to create markup languages such as DocBook.",
+                        "GlossSeeAlso": ["GML", "XML"]
+                                },
+                      "GlossSee": "markup"
+                            }
+                        }
+                    }
+                }
+            }
+            """;
+  
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
@@ -11,9 +36,15 @@ import Flutter
     let channel = FlutterMethodChannel(name: "DataChannel", binaryMessenger: controller.binaryMessenger)
         channel.setMethodCallHandler({
           (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
-          let data = stride(from: 0, to: 1000, by: 1).map { ("key\($0)", "value\($0)") }
-          
-          result(data.reduce(into: [:]) { $0[$1.0] = $1.1 })
+          if (call.method == "getData") {
+            let data = stride(from: 0, to: 1000, by: 1).map { ("key\($0)", self.json) }
+            
+            result(data.reduce(into: [:]) { $0[$1.0] = $1.1 })
+          } else if (call.method == "getImage") {
+            guard let image = UIImage(named: "image"),
+                  let data = image.jpegData(compressionQuality: 1.0) else { return }
+            result(FlutterStandardTypedData(bytes: data))
+          }
         })
     
     GeneratedPluginRegistrant.register(with: self)
